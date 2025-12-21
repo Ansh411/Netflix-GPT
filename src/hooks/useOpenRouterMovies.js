a79db10d15b82af8942aa9f91466d992a6f9fe35
Ansh411 <4gansh11@gmail.com>
2025-12-22 02:18:23 +0530

    Added a custom hook for AI Movie Results from OpenRouter

---
+++ src/hooks/useOpenRouterMovies.js
@@ -0,0 +1,32 @@
+import openrouter from "../utils/openRouter";
+
+const useOpenRouterMovies = () => {
+  const getMovieRecommendations = async (userQuery) => {
+    if (!userQuery) return [];
+
+    const prompt = `Return ONLY a comma-separated list of valid movie titles.
+                    No numbering.
+                    No explanations.
+                    No extra text.
+                    Give atleast 15 Movies
+                    User query: "${userQuery}"`;
+    try {
+      const response = await openrouter.chat.send({
+        model: "nex-agi/deepseek-v3.1-nex-n1:free",
+        messages: [{ role: "user", content: prompt }],
+      });
+
+      const movieText = response?.choices?.[0]?.message?.content || "";
+
+      return movieText.split(",").map((movie) => movie.trim()).filter(Boolean);
+      
+    } catch (error) {
+      console.error("OpenRouter error:", error);
+      return [];
+    }
+  };
+
+  return { getMovieRecommendations };
+};
+
+export default useOpenRouterMovies;
