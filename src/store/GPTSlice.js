de8e9fe1737bdff2f056ff5db35d1b3e3430eac1
Ansh411 <4gansh11@gmail.com>
2025-12-22 16:37:29 +0530

    Updated GPTSlice to get Loading State and User Query

--- src/store/GPTSlice.js
+++ src/store/GPTSlice.js
@@ -4,22 +4,33 @@ const GPTSlice = createSlice({
     name: "GPT",
     initialState : {
         showGPTSearch : false,
+        GPTSearchText : "",
         movieResults : null,
         movieNames : null,
+        isLoading: false,
     },
     reducers : {
         toggleGPTSearchView : (state) => {
             state.showGPTSearch = !state.showGPTSearch;
         },
-        addGPTMovieResult : (state, action) => {
 
+        startGPTLoading : (state) => {
+            state.isLoading = true;
+        },
+
+        addSearchText : (state, action) => {
+            state.GPTSearchText = action.payload;
+        },
+
+        addGPTMovieResult : (state, action) => {
             const { movieNames , movieResults } = action.payload;
             state.movieNames = movieNames;
             state.movieResults = movieResults;
-        }
+            state.isLoading = false;
+        },
     },
 });
 
-export const { toggleGPTSearchView, addGPTMovieResult } = GPTSlice.actions;
+export const { toggleGPTSearchView, startGPTLoading, addSearchText, addGPTMovieResult } = GPTSlice.actions;
 
 export default GPTSlice.reducer;
