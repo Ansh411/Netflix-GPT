c40065b2d4c5b4d897e9c6552db1f380b68473d2
Ansh411 <4gansh11@gmail.com>
2025-12-22 16:36:49 +0530

    Added a function to capitalize first letter of each word

---
+++ src/utils/capitalizeWords.js
@@ -0,0 +1,7 @@
+const capitalize = (str = "") => {
+
+  return str.toString().toLowerCase().split(" ").filter(Boolean).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+
+}
+
+export default capitalize;
