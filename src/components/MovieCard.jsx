f2f1cf2fff924d59c10e494f532ec415af4f74cf
Ansh411 <4gansh11@gmail.com>
2025-12-22 16:39:47 +0530

    Updated Movie Card for both GPT Page and Browse Page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,36 @@
+import { useSelector } from "react-redux";
 import { IMG_CDN_URL } from "../assets/constants";
 
 const MovieCard = ({ posterPath }) => {
+  const showGPTSearch = useSelector(store => store.GPT.showGPTSearch);
+
   if (!posterPath) return null;
 
   return (
-     <div className="min-w-40 md:min-w-[200px] lg:min-w-[220px] rounded-lg overflow-hidden transition duration-300 hover:scale-110 hover:z-20 shadow-lg cursor-pointer"> 
-      <img src={IMG_CDN_URL + posterPath} alt="Movie Poster" className="w-full h-full object-cover"/>
-    </div>
+    <>
+      {showGPTSearch ? (
+        /* 🔍 GPT SEARCH PAGE CARD */
+        <div className="w-[140px] sm:w-40 md:w-[180px] lg:w-[200px] rounded-xl overflow-hidden bg-zinc-900 shadow-xl transition-transform duration-300 hover:scale-110 cursor-pointer">
+          <img
+            src={IMG_CDN_URL + posterPath}
+            alt="Movie Poster"
+            className="w-full h-full object-cover"
+            loading="lazy"
+          />
+        </div>
+      ) : (
+        /* 🎬 BROWSE PAGE CARD (Horizontal scroll) */
+        <div
+          className=" min-w-40 md:min-w-[200px] lg:min-w-[220px] rounded-lg overflow-hidden shadow-lg transition duration-300 hover:scale-110 hover:z-20 cursor-pointer">
+          <img
+            src={IMG_CDN_URL + posterPath}
+            alt="Movie Poster"
+            className="w-full h-full object-cover"
+            loading="lazy"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
