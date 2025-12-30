eed69b5dcd1ce2a09d42cf27924872b3c441b0d5
Ansh411 <4gansh11@gmail.com>
2025-12-31 04:38:00 +0530

    Updated Main Container to get Random videos in play area

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,21 +1,29 @@
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import VideoBackground from "./VideoBackground";
 import VideoTitle from "./VideoTitle";
 
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
-  if (!movies?.length) return null;
+  const [mainMovie, setMainMovie] = useState(null);
 
-  const mainMovie = movies[1];
+  useEffect(() => {
+    if (!movies?.length) return;
+
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    setMainMovie(movies[randomIndex]);
+  }, [movies]);
+
+  if (!mainMovie) return null;
+
   const { title, overview, id } = mainMovie;
 
   return (
     <div className="relative h-screen w-full overflow-hidden">
       <VideoBackground movieId={id} />
-      <VideoTitle title={title} overview={overview} />
+      <VideoTitle title={title} movieId={id} overview={overview} />
     </div>
   );
 };
 
 export default MainContainer;
-
